333c48bb2fb7c39cbca40df1df77cee7a2d2c19f
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-07 19:08:34 +0100

    Add getShoppingList(recipe_id) helper function which returns a list of all ingredients and quantities for a given recipe

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -1,9 +1,30 @@
 const db = require("../data/db-config");
 
 module.exports = {
-  getRecipes
+  getRecipes,
+  getInstructions,
+  getShoppingList
+
 };
 
-function getRecipes() {
+function getRecipes(recipeId) {
   return db("recipes as r").select("r.id", "r.recipe_name");
 }
+
+function getInstructions(recipeId) {
+  return db("recipe_steps as rs")
+    .select("rs.step_number", "rs.instructions")
+    .where({ "rs.recipe_id": recipeId });
+}
+
+function getShoppingList(recipeId) {
+    return db("recipe_ingredients as ri")
+      .join("recipes as r", "ri.recipe_id", "=", "r.id")
+      .join("ingredients as i", "ri.ingredient_id", "=", "i.id")
+      .select("r.recipe_name", "i.ingredient_name", "ri.ingredient_quantity")
+      .where("r.id", recipeId);
+   }
+
+
+
+
--- recipe/recipe-router.js
+++ recipe/recipe-router.js
@@ -6,13 +6,45 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Recipes.getRecipes()
-  .then(schemes => {
-    res.json(schemes);
+  .then(recipes => {
+    res.status(200).json(recipes);
   })
   .catch(err => {
-    res.status(500).json({ message: 'Failed to get schemes' });
+    res.status(500).json({ message: 'Failed to get recipes' });
   });
 });
+
+router.get('/:id/instructions', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getInstructions(id)
+    .then(instructions => {
+      if (instructions) {
+        res.status(200).json(instructions);
+      } else {
+        res.status(404).json({ message: 'Could not find instructions with given recipe id.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipe instructions' });
+    });
+  });
+
+  router.get('/:id/shoppingList', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getShoppingList(id)
+    .then(shoppingList => {
+      if (shoppingList) {
+        res.status(200).json(shoppingList);
+      } else {
+        res.status(404).json({ message: 'Could not find shopping List with given recipe id.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipe instructions' });
+    });
+  });
 
 
 module.exports = router;
