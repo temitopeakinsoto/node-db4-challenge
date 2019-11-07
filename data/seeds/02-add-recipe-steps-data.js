dbb5f88a1b7c7751229814398376b1f1846427a8
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-07 17:51:58 +0100

    Add recipe_ingredients seed file
    In this commit, I created a seed file which is used for prepopulating the recipe_ingredients db table

--- data/seeds/02-add-recipe-steps-data.js
+++ data/seeds/02-add-recipe-steps-data.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('recipe_steps').insert([
-        {id: 1, step_number: 1, instructions:'Wash the rice', recipe_id:1},
-        {id: 1, step_number: 2, instructions:'Boil the rice', recipe_id:1},
-        {id: 1, step_number: 3, instructions:'Add Maggi and salt', recipe_id:1},
-        {id: 1, step_number: 4, instructions:'Serve while hot', recipe_id:1}
+        { step_number: 1, instructions:'Wash the rice', recipe_id:1},
+        { step_number: 2, instructions:'Boil the rice', recipe_id:1},
+        { step_number: 3, instructions:'Add Maggi and salt', recipe_id:1},
+        { step_number: 4, instructions:'Serve while hot', recipe_id:1}
       ]);
     });
 };
