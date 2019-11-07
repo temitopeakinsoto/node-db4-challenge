febf85d0851aff807d99a2a8865e600ea066c640
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-07 16:24:44 +0100

    Add recipe_steps seed file
    In this commit, I created a seed file which is used for prepopulating the recipe_steps db table
    Four recipe steps were added to the table and all four steps are steps involved in preparing a recipe with the id of 1

--- data/seeds/01-add-recipe-data.js
+++ data/seeds/01-add-recipe-data.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('recipe').truncate()
+  return knex('recipes').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('recipe').insert([
+      return knex('recipes').insert([
         {id: 1, recipe_name: 'banku'},
         {id: 2, recipe_name: 'akara'},
         {id: 3, recipe_name: 'efo riro'}
