6b2eafe899939f670d622fb1a2e13f1bc4cdfa9a
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-07 18:44:17 +0100

    Create getRecipes() function for returning a list of all the recipes in the database

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -1,28 +1,9 @@
 const db = require("../data/db-config");
 
 module.exports = {
-  getRecipes,
-  getShoppingList,
-  getInstructions
+  getRecipes
 };
 
 function getRecipes() {
   return db("recipes as r").select("r.id", "r.recipe_name");
 }
-
-// function getShoppingList(schemeId) {
-//   return db("schemes as s").first()
-//     .select("s.id", "s.scheme_name")
-//     .where({ "s.id": schemeId });
-// }
-
-// function getInstructions(schemeId) {
-//   return db("steps as s")
-//     .join("schemes", "schemes.id", "s.scheme_id")
-//     .select("s.id", "s.instructions", "scheme_name")
-//     .where({ "s.scheme_id": schemeId });
-// }
-
-
-
-
